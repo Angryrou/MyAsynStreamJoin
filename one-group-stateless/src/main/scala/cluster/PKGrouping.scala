f602d0d66f3be0c78b2b237e725f65203f66ac35
Kawhiii <chenghaolv@163.com>
2017-09-22 19:44:20 +0800

    [stateless - groupBy] modify PK HH RR

--- one-group-stateless/src/main/scala/cluster/PKGrouping.scala
+++ one-group-stateless/src/main/scala/cluster/PKGrouping.scala
@@ -15,13 +15,14 @@ import scala.collection.mutable
 object PKGrouping {
   def main(args: Array[String]) {
     if (args.length != 2) {
-      System.err.println("Usage: PKGrouping_stateless <stream.json> 1,2")
+      System.err.println("Usage: PKGrouping_stateless <stream.json> duplicateRate")
       System.exit(1)
     }
     // 参数读取
     val (brokers, topics, batch_duration, ports_num, m, r, kafka_offset, path, lgw, key_space, sleep_time_map_ns,
     sleep_time_reduce_ns) = MyUtils.getFromJson(args(0))
-    val seeds= args(1).split(",").map(_.toInt)
+    val seeds = Array(1,2)
+    val duplicateRate = Integer.parseInt(args(1))
 
     // new 一个 streamingContext
     val sc = new SparkConf().setAppName("PKGrouping_stateless")
@@ -54,16 +55,36 @@ object PKGrouping {
       ret.iterator
     }
 
+    // input: "timestamp AAA 999" (ts, z, x) 均来自同一个 relation,所以 timestamp 的数据有序
+    // output: (z, x)
+    val preProcess = (iter : Iterator[String]) => {
+      val ret = mutable.ListBuffer[(String, Int)]() // return type
+      while (iter.hasNext) {
+        val tmp = iter.next().split(' ')
+        val z = tmp(1)
+        val x = tmp(2).toInt
+        for (a <- 1 to duplicateRate) {
+          ret += (z -> x)
+        }
+      }
+      ret.iterator
+    }
+
+    // (porti, "ts z x;*;*;*")
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topics)
-      .flatMap(_._2.split(";"))
-      .map(w => (w, 1)) // (word, 1)
+      .flatMap(_._2.split(";")) // "ts z x"
+      .mapPartitions(preProcess)
       .transform(_.partitionBy(new PKPartitioner(m, seeds)))
       .mapPartitions(mapLocalCompute)
       .transform(_.partitionBy(new HashPartitioner(r)))
       .mapPartitions(reduceLocalCompute)
 
-    messages.foreachRDD(_.collect())
+    messages.foreachRDD((rdd, time) => {
+      rdd.foreach(println)
+      println(s"----- $time -----")
+      println()
+    })
     ssc.start()
     ssc.awaitTermination()
   }
--- one-group-stateless/src/main/scala/cluster/RRGrouping.scala
+++ one-group-stateless/src/main/scala/cluster/RRGrouping.scala
@@ -2,7 +2,7 @@ package cluster
 
 import kafka.serializer.StringDecoder
 import myutils.MyUtils
-import org.apache.spark.{HashPartitioner, SparkConf}
+import org.apache.spark.{HashPartitioner, SparkConf, TaskContext}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import partitioner.RRPartitioner
@@ -15,13 +15,14 @@ import scala.collection.mutable
 object RRGrouping {
 
   def main(args: Array[String]) {
-    if (args.length != 1) {
-      System.err.println("Usage: RRGrouping_stateless <stream.json>")
+    if (args.length != 2) {
+      System.err.println("Usage: RRGrouping_stateless <stream.json> duplicateRate")
       System.exit(1)
     }
     // 参数读取
     val (brokers, topics, batch_duration, ports_num, m, r, kafka_offset, path, lgw, key_space, sleep_time_map_ns,
     sleep_time_reduce_ns) = MyUtils.getFromJson(args(0))
+    val duplicateRate = Integer.parseInt(args(1))
 
     // new 一个 streamingContext
     val sc = new SparkConf().setAppName("RRGrouping_stateless")
@@ -54,16 +55,36 @@ object RRGrouping {
       ret.iterator
     }
 
+    // input: "timestamp AAA 999" (ts, z, x) 均来自同一个 relation,所以 timestamp 的数据有序
+    // output: (z, x)
+    val preProcess = (iter : Iterator[String]) => {
+      val ret = mutable.ListBuffer[(String, Int)]() // return type
+      while (iter.hasNext) {
+        val tmp = iter.next().split(' ')
+        val z = tmp(1)
+        val x = tmp(2).toInt
+        for (a <- 1 to duplicateRate) {
+          ret += (z -> x)
+        }
+      }
+      ret.iterator
+    }
+
+    // (porti, "ts z x;*;*;*")
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topics)
-      .flatMap(_._2.split(";"))
-      .map(w => (w, 1)) // (word, 1)
+      .flatMap(_._2.split(";")) // "ts z x"
+      .mapPartitions(preProcess)
       .transform(_.partitionBy(new RRPartitioner(m)))
       .mapPartitions(mapLocalCompute)
       .transform(_.partitionBy(new HashPartitioner(r)))
       .mapPartitions(reduceLocalCompute)
 
-    messages.foreachRDD(_.collect())
+    messages.foreachRDD((rdd, time) => {
+      rdd.foreach(println)
+      println(s"----- $time -----")
+      println()
+    })
     ssc.start()
     ssc.awaitTermination()
   }
